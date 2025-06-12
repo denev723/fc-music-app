c5e9075421ae7a0e07daa42a40c3c3377125689e
denev <denev723@gmail.com>
2025-06-12 19:06:36 +0900

    slider 컴포넌트 추가

--- packages/fc-music-client/src/App.tsx
+++ packages/fc-music-client/src/App.tsx
@@ -1,20 +1,33 @@
 import useGetSongs from "@/hooks/useGetSongs";
 import ErrorFallback from "@/presentionals/common/ErrorFallback";
 import RootLayout from "@/presentionals/common/RootLayout";
+import SliderPanel from "@/presentionals/common/SliderPanel";
 import SongCard from "@/presentionals/common/SongCard";
 import PlayerWrapper from "@/presentionals/player/PlayerWrapper";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RootLayout>
+        <button onClick={handleOpen}>Open</button>
         <ErrorBoundary FallbackComponent={ErrorFallback}>
           <TempComponent />
         </ErrorBoundary>
+        <SliderPanel open={open} onClose={handleClose}>
+          <div className="w-[300px]">
+            <h1>재생목록</h1>
+          </div>
+        </SliderPanel>
       </RootLayout>
       <PlayerWrapper />
     </QueryClientProvider>
---
+++ packages/fc-music-client/src/hooks/common/useOutsideClick.ts
@@ -0,0 +1,25 @@
+import { useEffect, useRef } from "react";
+
+export default function useOutsideClick<RefType extends HTMLElement>(
+  callback: () => void
+) {
+  const ref = useRef<RefType>(null);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent | TouchEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClick);
+    document.addEventListener("touchstart", handleClick);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClick);
+      document.removeEventListener("touchstart", handleClick);
+    };
+  }, [callback]);
+
+  return ref;
+}
