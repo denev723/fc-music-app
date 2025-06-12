880aeaa453ef1e4b32429ad3e0c9207174c1f4e3
denev <denev723@gmail.com>
2025-06-12 18:49:26 +0900

    card component

--- packages/fc-music-client/src/App.tsx
+++ packages/fc-music-client/src/App.tsx
@@ -1,6 +1,7 @@
 import useGetSongs from "@/hooks/useGetSongs";
 import ErrorFallback from "@/presentionals/common/ErrorFallback";
 import RootLayout from "@/presentionals/common/RootLayout";
+import SongCard from "@/presentionals/common/SongCard";
 import PlayerWrapper from "@/presentionals/player/PlayerWrapper";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary } from "react-error-boundary";
@@ -23,7 +24,19 @@ function App() {
 function TempComponent() {
   const { data } = useGetSongs();
 
-  return <div>{data ? JSON.stringify(data) : "loading..."}</div>;
+  return (
+    <div className="flex gap-x-20">
+      {data?.map((song) => (
+        <SongCard key={song.id}>
+          <SongCard.Image src={"https://placehold.co/150"} alt={song.title} />
+          <SongCard.Content>
+            <SongCard.Title>{song.title}</SongCard.Title>
+            <SongCard.Description>{song.artist}</SongCard.Description>
+          </SongCard.Content>
+        </SongCard>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- packages/fc-music-client/src/__mock__/handlers.ts
+++ packages/fc-music-client/src/__mock__/handlers.ts
@@ -3,23 +3,29 @@ import { graphql, HttpResponse } from "msw";
 export const hanlders = [
   graphql.query("GetSongs", () => {
     return HttpResponse.json({
-      errors: [
-        {
-          message: "Internal server error",
-        },
-      ],
+      data: {
+        songs: [
+          {
+            id: 1,
+            title: "Song 1",
+            artist: "Artist 1",
+            genre: "rock",
+          },
+          {
+            id: 2,
+            title: "Song 2",
+            artist: "Artist 2",
+            genre: "ballad",
+          },
+        ],
+      },
     });
     // return HttpResponse.json({
-    //   data: {
-    //     songs: [
-    //       {
-    //         id: 1,
-    //         title: "Song 1",
-    //         artist: "Artist 1",
-    //         genre: "rock",
-    //       },
-    //     ],
-    //   },
+    //   errors: [
+    //     {
+    //       message: "Internal server error",
+    //     },
+    //   ],
     // });
   }),
 ];
